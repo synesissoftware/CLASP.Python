ff39325f0bd99c2e573e62cf70ffb620b0cf4da9
Matt <matthew@synesis.com.au>
2020-07-07 14:20:59 +1000

    + added build_egg.sh

---
+++ build_egg.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# ######################################################################## #
+# File:         build_egg.sh
+#
+# Purpose:      Builds the egg
+#
+# Created:      7th July 2020
+# Updated:      7th July 2020
+#
+# Author:       Matthew Wilson
+#
+# Copyright:    <<TBD>>
+#
+# ######################################################################## #
+
+python3 setup.py sdist
+
