bd7c56e5347ce41629055a01b887bef8990b1d05
Matt Wilson <matthew.wilson.synesis@gmail.com>
2026-07-24 07:37:48 +1000

    chore: Python 2.7 compatibility

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,23 @@
-# This file present to ensure that unittest's discover operates recursively
+# This file present to deal with warnings from packager
+
+import sys
+
+if sys.version_info[0] < 3:
+
+    import unittest
+
+    try:
+
+        import mock
+
+        sys.modules['unittest.mock'] = mock
+    except ImportError:
+
+        raise ImportError(
+            "the 'mock' package is required to run tests on Python 2.7",
+        )
+
+    if not hasattr(unittest.TestCase, 'assertRegex'):
+
+        unittest.TestCase.assertRegex = unittest.TestCase.assertRegexpMatches
 
